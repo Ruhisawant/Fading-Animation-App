6f509639ccf27ace8accb67cb151e6516dfd7658
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-02-26 18:43:57 -0500

    Added darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,27 @@ class MyApp extends StatelessWidget {
 
   @override
     Widget build(BuildContext context) {
-    return const MaterialApp(
-    home: FadingTextAnimation(),
+      return const MaterialApp(
+        home: FadingTextAnimation(),
     );
   }
 }
 
 class FadingTextAnimation extends StatefulWidget {
   const FadingTextAnimation({super.key});
+
   @override
   FadingTextAnimationState createState() => FadingTextAnimationState();
 }
 class FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
 
   void toggleVisibility() {
     setState(() {
@@ -31,23 +39,47 @@ class FadingTextAnimationState extends State<FadingTextAnimation> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Fading Text Animation'),
-      ),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: const Duration(seconds: 1),
-          child: const Text(
-          'Hello, Flutter!',
-          style: TextStyle(fontSize: 24),
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fading Text Animation'),
+          actions: const[
+            Icon(Icons.dark_mode, color: Colors.white, size: 30),
+          ],
+        ),
+        body: Center(
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: const Duration(seconds: 1),
+            child: const Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24),
+            ),
           ),
         ),
+        floatingActionButton: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                if (_themeMode == ThemeMode.dark) {
+                  _themeMode = ThemeMode.light;
+                } else {
+                  _themeMode = ThemeMode.dark;
+                }
+              });
+            },
+            child: Icon(Icons.dark_mode, color: Colors.white),
+          ),
+          FloatingActionButton(
+            onPressed: toggleVisibility,
+            child: Icon(Icons.play_arrow),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-      onPressed: toggleVisibility,
-      child: const Icon(Icons.play_arrow),
       ),
     );
   }
